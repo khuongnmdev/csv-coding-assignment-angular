130196a6cabdd4eff5a2d03c52cc32bb2fa7850a
Khuong Nguyen <khuongnm.dev@gmail.com>
2023-03-03 15:59:13 +0700

    Binding value to Task Detail component

--- src/app/app-routing.modules.ts
+++ src/app/app-routing.modules.ts
@@ -1,11 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { TaskDetailComponent } from "./task-detail/task-detail.component";
 import { TaskListComponent } from "./task-list/task-list.component";
+import { TaskDetailComponent, TASK_ID_PARAM_KEY } from "./task-detail/task-detail.component";
 
 export const routes: Routes = [
   { path: '', component: TaskListComponent },
-  { path: 'detail/:id', component: TaskDetailComponent },
+  { path: `detail/:${TASK_ID_PARAM_KEY}`, component: TaskDetailComponent },
   { path: '**', redirectTo: '' },
 ];
 
@@ -13,4 +13,4 @@ export const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/task-detail/task-detail.component.ts
+++ src/app/task-detail/task-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService, Task, User } from '../backend.service';
+import { EMPTY, Observable, iif } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { shareReplay, switchMap, switchMapTo } from 'rxjs/operators';
+
+export const TASK_ID_PARAM_KEY = 'userId';
 
 @Component({
   selector: 'app-task-detail',
@@ -7,7 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskDetailComponent implements OnInit {
 
-  constructor() { }
+  /** User to display. */
+  protected readonly task$: Observable<Task>;
+  protected readonly user$: Observable<User>;
+
+  constructor(
+    activatedRoute: ActivatedRoute,
+    private backend: BackendService
+  ) {
+    const taskId = activatedRoute.snapshot.paramMap.get(TASK_ID_PARAM_KEY);
+    this.task$ = this.backend.task(Number(taskId)).pipe(
+      shareReplay({ refCount: true, bufferSize: 1 }),
+    );
+
+    this.user$ = this.task$.pipe(switchMap(task => iif(() => task.assigneeId !== null,
+      this.backend.user(Number(task.assigneeId),
+      ))))
+
+  }
 
   ngOnInit() {
   }
